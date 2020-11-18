e6c564bc11e1b04b96d70e490c8d82994f52ecdf
FehnkerA <fehnker@gmail.com>
2020-11-18 10:18:25 +0100

    Separate box.py file

---
+++ exercises/box.py
@@ -0,0 +1,7 @@
+class Box:
+    """Class for the bx that constraints the bubble, most of the time."""
+
+    def __init__(self):
+        pass
+
+
--- exercises/bubble.py
+++ exercises/bubble.py
@@ -7,13 +7,6 @@ class Bubble:
         pass
 
 
-class Box:
-    """Class for the bx that constraints the bubble, most of the time."""
-
-    def __init__(self):
-        pass
-
-
 
 
 
